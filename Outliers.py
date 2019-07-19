703d267be47b1227cc5d576c692d06140b34682a
chenlu <chenlu2394349937@163.com>
2019-07-19 14:05:59 +0800

    change dbscan

--- Outliers.py
+++ Outliers.py
@@ -106,7 +106,7 @@ class Outliers:
             return _inliers, _inliers_index, _outliers
 
     def dbscan(data, dimension=[], eps=0.5, min_samples=5, plot=False):
-        predict = data
+        predict = data.copy()
         predict = pd.DataFrame(predict)
         # 检查是否传入维度参数
         if len(dimension) == 0:
@@ -119,68 +119,78 @@ class Outliers:
         else:
             clu = DBSCAN(eps, min_samples)
             clu.fit(predict)
-            core_inliers_index = clu.core_sample_indices_  # 属性core_sample_indices_返回核心点索引
+            core_inliers_index = set(clu.core_sample_indices_.tolist())  # 属性core_sample_indices_返回核心点索引
+            # print(core_inliers_index)
             labels = clu.labels_  # 获取簇的标签，属于标签-1的为异常点
             predict['labels'] = labels
+            # 获取正常点索引
             _inliers_index = predict[predict['labels'] != -1].index.tolist()
-            unique_labels = list(set(labels))
-            # 获取不同簇的数据
-            n = len(unique_labels)  # 获取簇的个数,所有离群点为一簇
-            # 检查输入数据集是否为二维
-            if np.shape(predict)[1] - 1 == 2:  # 由于加了一列存储分类标签，所以需要-1
-                # 检查传入参数plot,是否有绘图需要
-                if plot is True:
-                    # 给不同簇中的数据点分配不同的颜色
-                    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, n)]
-                    # 定义一个list用来保存colors.py文件内对不同颜色设置的字符
-                    for k, col in zip(unique_labels, colors):
-                        if k == -1:
-                            # 红色用于离群点上
-                            plt.plot(predict[predict['labels'] == k].iloc[:, 0],
-                                     predict[predict['labels'] == k].iloc[:, 1],
-                                     'o',
-                                     markerfacecolor='r',
-                                     markeredgecolor='r', markersize=3)
-                        else:
-                            predict_plot = predict[predict['labels'] == k]
-                            core_plot = []
-                            notcore_plot = []
-                            for j in predict_plot.index.tolist():
-                                # 将簇中的核心点和非核心点分开，核心点大，非核心点小，但同一簇中点颜色相同
-                                if j in core_inliers_index:
-                                    core_plot.append(j)
-                                else:
-                                    notcore_plot.append(j)
-                                plt.plot(predict_plot.loc[core_plot, 0],
-                                         predict_plot.loc[core_plot, 1],
-                                         'o',
-                                         markerfacecolor=tuple(col),
-                                         markeredgecolor=tuple(col), markersize=6)
-                                plt.plot(predict_plot.loc[notcore_plot, 0],
-                                         predict_plot.loc[notcore_plot, 1],
-                                         'o',
-                                         markerfacecolor=tuple(col),
-                                         markeredgecolor=tuple(col), markersize=3)
-                else:
-                    pass
-                plt.show()
-            else:
-                pass
+            # print(_inliers_index)
             # 获取离群值的索引
             _outliers_index = predict[predict['labels'] == -1].index.tolist()
-            _outliers = predict.iloc[_outliers_index, :]
-            _inliers = predict.iloc[_inliers_index, :]
+            _outliers = predict.iloc[_outliers_index, :-1]
+            _inliers = predict.iloc[_inliers_index, :-1]
             if type(data) == list:
                 data_1 = np.array(data)
-                _outliers = data_1[_outliers_index, :]
-                _inliers = data_1[_inliers_index, :]
-                _outliers = _outliers.tolist()
-                _inliers = _inliers.tolist()
+                _outliers = data_1[_outliers_index, :].tolist()
+                _inliers = data_1[_inliers_index, :].tolist()
             if type(data) == np.ndarray:
                 _outliers = data[_outliers_index, :]
                 _inliers = data[_inliers_index, :]
             if type(data) == pd.DataFrame:
-                data = data.iloc[:, :-1]
                 _outliers = data.iloc[_outliers_index, :]
                 _inliers = data.iloc[_inliers_index, :]
+            unique_labels = list(set(labels))
+            unique_labels.remove(-1)
+            n = len(unique_labels)  # 获取正常点簇的个数
+            # 检查输入数据集是否为二维
+            if np.shape(predict)[1] - 1 == 2:  # 由于加了一列存储分类标签，所以需要-1
+                # 检查传入参数plot,是否有绘图需要
+                if plot is True:
+                    # 标红离群点
+                    plt.plot(predict.iloc[_outliers_index, 0],
+                             predict.iloc[_outliers_index, 1],
+                             'o',
+                             markerfacecolor='r',
+                             markeredgecolor='r', markersize=3)
+                    # 给不同簇中的数据点分配不同的颜色
+                    colors = [plt.cm.Spectral(each) for each in
+                              np.linspace(0, 1, n)]  # 定义一个list用来保存colors.py文件内对不同颜色设置的字符
+                    for k, col in zip(unique_labels, colors):
+                        # 不区分核心点与非核心点的大小
+                        '''
+                        plt.plot(predict[predict['labels'] == k].iloc[:, 0],
+                                 predict[predict['labels'] == k].iloc[:, 1],
+                                 'o',
+                                 markerfacecolor=tuple(col),
+                                 markeredgecolor=tuple(col), markersize=3)
+                        '''
+                        # 区分核心点与非核心点的大小
+                        plot_index = predict[predict['labels'] == k].index.tolist()
+                        # print(plot_index)
+                        color = tuple(col)
+                        core_plot = []
+                        notcore_plot = []
+                        for j in plot_index:
+                            # 同簇数据点颜色相同，同簇内核心点较大，非核心点较小
+                            if j in core_inliers_index:
+                                core_plot.append(j)
+                            else:
+                                notcore_plot.append(j)
+                        plt.plot(predict.iloc[core_plot, 0],
+                                 predict.iloc[core_plot, 1],
+                                 'o',
+                                 markerfacecolor=color,
+                                 markeredgecolor=color, markersize=3)
+                        plt.plot(predict.iloc[notcore_plot, 0],
+                                 predict.iloc[notcore_plot, 1],
+                                 'o',
+                                 markerfacecolor=color,
+                                 markeredgecolor=color, markersize=1)
+                    plt.show()
+                else:
+                    pass
+            else:
+                pass
+
             return _inliers, _inliers_index, _outliers
